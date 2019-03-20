961e0cd0b282d7a2a143b2109e48432a1ae5a3bd
oitsjustjose <stovallj1995@gmail.com>
2019-03-20 14:55:13 -0400

    Reformat, bump version

--- setup.py
+++ setup.py
@@ -7,21 +7,19 @@ def main():
     with open("README.md", "r") as fh:
         long_description = fh.read()
 
-    setuptools.setup(name="flask_opencv_streamer",
-                     version='1.1',
-                     url='https://github.com/oitsjustjose/Flask-OpenCV-Streamer',
-                     author='Jose Stovall',
-                     author_email='stovallj1995@gmail.com',
-                     description="A Python package for easily streaming OpenCV footage, even with authentication",
-                     long_description=long_description,
-                     long_description_content_type="text/markdown",
-                     license='GNU General Public License 3',
-                     packages=setuptools.find_packages(),
-                     install_requires=[
-                         'cryptography',
-                         'flask',
-                         'opencv-python'
-                     ])
+    setuptools.setup(
+        name="flask_opencv_streamer",
+        version="1.2",
+        url="https://github.com/oitsjustjose/Flask-OpenCV-Streamer",
+        author="Jose Stovall",
+        author_email="stovallj1995@gmail.com",
+        description="A Python package for easily streaming OpenCV footage, even with authentication",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        license="GNU General Public License 3",
+        packages=setuptools.find_packages(),
+        install_requires=["cryptography", "flask", "opencv-python"],
+    )
 
 
 if __name__ == "__main__":
